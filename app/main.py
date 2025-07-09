61b989a3f5f816be759622afed0b411b1361c503
rosenascimento <nascimento.roseane12@gmail.com>
2025-07-09 17:19:45 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from database import setup_database
 app = FastAPI()
 
 @app.get("/health")
-def health_check():
+def health():
     return {"status": "ok"}
 
 app.add_middleware(
@@ -23,4 +23,7 @@ app.include_router(scan_routes)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
