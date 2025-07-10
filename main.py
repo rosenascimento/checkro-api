e198f615a6d99ee0a54e58388b98860179cc86d9
rosenascimento <nascimento.roseane12@gmail.com>
2025-07-10 15:32:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+   allow_origins=["https://checkro.com"],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
