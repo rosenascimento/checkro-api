3202022ef0d28abca4b10f15f1afdf8243bb5e15
rosenascimento <nascimento.roseane12@gmail.com>
2025-07-10 21:19:53 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 # models.py
 
 from pydantic import BaseModel
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
 
@@ -11,14 +14,10 @@ from datetime import datetime
 class ScanRequest(BaseModel):
     url: str
 
-# Base SQLAlchemy
-Base = declarative_base()
-
 # Modelo SQLAlchemy (para o banco de dados)
 class Scan(Base):
     __tablename__ = "scans"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     url = Column(String, nullable=False)
-    status = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
