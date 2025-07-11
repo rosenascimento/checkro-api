7e05437ce8e4ec4c77b50bb33b5f99ad4809d20c
rosenascimento <nascimento.roseane12@gmail.com>
2025-07-11 15:56:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = FastAPI()
+
+# CORS CONFIG
+origins = [
+    "https://checkro.com",  # Permite frontend oficial
+    "http://localhost:3000",  # Permite desenvolvimento local (opcional)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health():
